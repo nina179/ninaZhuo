46cafb4a733df72411f7466cf8207531702a7182
nina <nzhuo1@swarthmore.edu>
2024-11-14 14:19:24 -0500

    add nav

--- components/nav.js
+++ components/nav.js
@@ -5,17 +5,16 @@ class Nav extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <header>
-          <nav>
+        <nav style="width:20%; padding-right: 30vw;">
             <ul>
-              <li><a href="about.html">About</a></li>
-              <li><a href="work.html">Work</a></li>
-              <li><a href="contact.html">Contact</a></li>
+                <li style="width: 200%; line-height: 1;"><a src="/index.html"><h1>Nina Zhuo</h1></a></li>
+                <li><a src="/pages/about.html">About</a></li>
+                <li><a src="/pages/portfolio.html">Portfolio</a></li>
+                <li><a src="/pages/blog.html">Blog</a></li>
             </ul>
-          </nav>
-        </header>
+        </nav>
       `;
     }
   }
 
-costumElements.define('nav-component', Nav);
+customElements.define('nav-component', Nav);
